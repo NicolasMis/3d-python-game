7aaafacb99e49e617a332574b18003671153e506
Nicolas Klug <nicolas_klug@mis-munich.de>
2024-12-13 09:57:29 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
 import pygame 
 import pyopenxr as xr
+
+pygame.init()
+
+screen_width = 800
+screen_length = 600
+
+screen = pygame.display.set_mode((screen_width, screen_length))
+
+run= True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
